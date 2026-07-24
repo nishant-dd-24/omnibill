2309b2562ed270ac9f2153403f70b714ef5f85e5
Nishant Dwivedi <nishant.dd.2484@gmail.com>
2026-07-24 18:49:43 +0530

    test(rls): restore PostgreSQL tenant context during tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,6 +20,28 @@ use Modules\Tenant\Domain\Models\Tenant;
  */
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        try {
+            $rlsManager = app(\Modules\Tenant\Infrastructure\Database\PostgresRlsManager::class);
+            $rlsManager->bypassRls();
+            
+            \Illuminate\Support\Facades\Event::listen(\Illuminate\Foundation\Http\Events\RequestHandled::class, function () use ($rlsManager) {
+                $rlsManager->bypassRls();
+            });
+            \Illuminate\Support\Facades\Event::listen(\Illuminate\Queue\Events\JobProcessed::class, function () use ($rlsManager) {
+                $rlsManager->bypassRls();
+            });
+            \Illuminate\Support\Facades\Event::listen(\Illuminate\Queue\Events\JobFailed::class, function () use ($rlsManager) {
+                $rlsManager->bypassRls();
+            });
+        } catch (\Throwable $e) {
+            // Ignore if class does not exist or fails to resolve
+        }
+    }
+
     protected function tearDown(): void
     {
         Context::flush();
